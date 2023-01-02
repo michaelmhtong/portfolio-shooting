f90d0b32223769ad2502951dede6337d2de99a58
michael07200 <mickmhtong@gmail.com>
2023-01-02 22:30:05 +0100

    build: colorize game

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "blue");
+const player = new Player(x, y, 10, "white");
 const projectiles = [];
 const enemies = [];
 
@@ -88,7 +88,7 @@ function spawnEnemies() {
       x = Math.random() * canvas.width;
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
-    const color = "green";
+    const color = `hsl(${Math.random() * 360},50%,50%)`;
     const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
     const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
     enemies.push(new Enemy(x, y, radius, color, velocity));
@@ -98,10 +98,24 @@ function spawnEnemies() {
 let animationId;
 function animate() {
   animationId = requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = "rgba(0,0,0,0.1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    // remove the projectile if it is out of window
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        // delete the projectile
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
   enemies.forEach((enemy, index) => {
     enemy.update();
@@ -127,9 +141,10 @@ function animate() {
 }
 
 addEventListener("click", (event) => {
+  console.log(projectiles);
   const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
-  const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
-  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity));
+  const velocity = { x: Math.cos(angle) * 5, y: Math.sin(angle) * 5 };
+  projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity));
 });
 
 animate();
